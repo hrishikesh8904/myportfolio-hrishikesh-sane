f7b94aea458f4c682978e33911cac3b7d31087b7
hrishikesh8904 <hrishikesh.sane89@outlook.com>
2024-09-12 03:51:59 +0530

    modified:   portfolio/package-lock.json 	modified:   portfolio/package.json 	modified:   portfolio/src/App.css 	modified:   portfolio/src/App.js 	new file:   portfolio/src/assets/favicon.ico 	new file:   portfolio/src/components/About.js 	new file:   portfolio/src/components/Education.js 	new file:   portfolio/src/components/Experiance.js 	new file:   portfolio/src/components/Home.js 	new file:   portfolio/src/components/Navbar.js 	new file:   portfolio/src/components/Skills.js 	deleted:    portfolio/src/logo.svg

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar.js";
+import Home from "./components/Home.js";
+import About from "./components/About.js";
+import Skills from "./components/Skills.js";
+import Education from "./components/Education.js";
+import Experiance from "./components/Experiance.js";
+import { Link, Element } from "react-scroll";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar></Navbar>
+        <div className="sections">
+          <Element name="Home">
+            <Routes>
+              <Route path="/" exact component={Home}></Route>
+            </Routes>
+          </Element>
+          <Element name="about">
+            <Routes>
+              <Route path="/about" component={About}></Route>
+            </Routes>
+          </Element>
+          <Element name="skills">
+            <Routes>
+              <Route path="/skills" component={Skills}></Route>
+            </Routes>
+          </Element>
+          <Element name="experiance">
+            <Routes>
+              <Route path="/experiance" component={Experiance}></Route>
+            </Routes>
+          </Element>
+          <Element name="education">
+            <Routes>
+              <Route path="/education" component={Education}></Route>
+            </Routes>
+          </Element>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,109 @@
+import "../App.css";
+import "../../node_modules/bootstrap/dist/css/bootstrap.css";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/favicon.ico";
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import { CgGitFork } from "react-icons/cg";
+function NavBar() {
+  const [expand, updateExpanded] = useState(false);
+  const [navColour, updateNavbar] = useState(false);
+  function scrollHandler() {
+    if (window.scrollY >= 20) {
+      updateNavbar(true);
+    } else {
+      updateNavbar(false);
+    }
+  }
+  window.addEventListener("scroll", scrollHandler);
+  return (
+    <Navbar
+      bg="myWhite"
+      expanded={expand}
+      fixed="top"
+      expand="md"
+      className={navColour ? "sticky" : "navbar"}
+    >
+      <Container>
+        <Navbar.Brand>
+          <img
+            src={logo}
+            alt="Brand"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            draggable="false"
+          />{" "}
+          Hrishikesh
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          onClick={() => {
+            updateExpanded(expand ? false : "expanded");
+          }}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto" defaultActiveKey="#home">
+            <Nav.Item>
+              <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
+                Home
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/about"
+                onClick={() => updateExpanded(false)}
+              >
+                About
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/skills"
+                onClick={() => updateExpanded(false)}
+              >
+                Skills
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/experiance"
+                onClick={() => updateExpanded(false)}
+              >
+                Experiance
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/education"
+                onClick={() => updateExpanded(false)}
+              >
+                Education
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="fork-btn">
+              <Button
+                href="https://github.com/hrishikesh8904"
+                target="_blank"
+                className="fork-btn-inner"
+              >
+                <CgGitFork style={{ fontSize: "1.2rem" }}></CgGitFork>
+              </Button>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
